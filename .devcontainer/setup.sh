83cbbee39bfd2672578f58bdaaac07ea93a80ad7
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2025-11-02 11:47:36 +0000

    追加

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y git-lfs
+git-lfs install
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/midnightntwrk/compact/releases/latest/download/compact-installer.sh | sh
+compact update
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
