7123cb39f79ffc086924503bb910bf492546d2d6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-11-07 22:31:10 +0900

    fix

--- frontend-vite-react/src/modules/midnight/counter-ui/contexts/counter-deployment.tsx
+++ frontend-vite-react/src/modules/midnight/counter-ui/contexts/counter-deployment.tsx
@@ -1,28 +1,41 @@
+import { type Logger } from "pino";
 import type { PropsWithChildren } from "react";
 import { createContext, useMemo } from "react";
-import { type Logger } from "pino";
 
-import type { DeployedAPIProvider } from "./counter-deployment-class";
-import { useLocalState } from "../hooks/use-localStorage";
-import { DeployedTemplateManager } from "./counter-deployment-class";
-import { useProviders } from "../hooks";
 import type { ContractAddress } from "@midnight-ntwrk/compact-runtime";
+import { useProviders } from "../hooks";
+import { useLocalState } from "../hooks/use-localStorage";
+import type { DeployedAPIProvider } from "./counter-deployment-class";
+import { DeployedTemplateManager } from "./counter-deployment-class";
 
-export const DeployedProviderContext = createContext<DeployedAPIProvider | undefined>(undefined);
+export const DeployedProviderContext = createContext<
+  DeployedAPIProvider | undefined
+>(undefined);
 
 export type DeployedProviderProps = PropsWithChildren<{
   logger: Logger;
   contractAddress: ContractAddress;
 }>;
 
-export const DeployedProvider = ({ logger, contractAddress, children }: DeployedProviderProps) => {
+export const DeployedProvider = ({
+  logger,
+  contractAddress,
+  children,
+}: DeployedProviderProps) => {
   const localState = useLocalState();
   const providers = useProviders();
   const manager = useMemo(() => {
-    return new DeployedTemplateManager(logger, localState, contractAddress, providers?.providers);
+    return new DeployedTemplateManager(
+      logger,
+      localState,
+      contractAddress,
+      providers?.providers
+    );
   }, [logger, localState, providers?.providers]);
 
   return (
-    <DeployedProviderContext.Provider value={manager}>{children}</DeployedProviderContext.Provider>
+    <DeployedProviderContext.Provider value={manager}>
+      {children}
+    </DeployedProviderContext.Provider>
   );
 };
--- frontend-vite-react/src/modules/midnight/counter-ui/contexts/index.tsx
+++ frontend-vite-react/src/modules/midnight/counter-ui/contexts/index.tsx
@@ -1,14 +1,14 @@
+import type { ContractAddress } from "@midnight-ntwrk/compact-runtime";
+import { Logger } from "pino";
 import { DeployedProvider } from "./counter-deployment";
 import { LocalStorageProvider } from "./counter-localStorage";
 import { Provider } from "./counter-providers";
-import { Logger } from "pino";
-import type { ContractAddress } from "@midnight-ntwrk/compact-runtime";
 
-export * from "./counter-providers";
-export * from "./counter-localStorage";
-export * from "./counter-localStorage-class";
 export * from "./counter-deployment";
 export * from "./counter-deployment-class";
+export * from "./counter-localStorage";
+export * from "./counter-localStorage-class";
+export * from "./counter-providers";
 
 interface AppProviderProps {
   children: React.ReactNode;
@@ -16,7 +16,11 @@ interface AppProviderProps {
   contractAddress: ContractAddress;
 }
 
-export const CounterAppProvider = ({ children, logger, contractAddress }: AppProviderProps) => {
+export const CounterAppProvider = ({
+  children,
+  logger,
+  contractAddress,
+}: AppProviderProps) => {
   return (
     <LocalStorageProvider logger={logger}>
       <Provider logger={logger}>
